7b1ebc14d60f50d89e7abe6d6a26dba0386fb0b2
Dani <dshemesh@newrelic.com>
2021-03-21 13:36:44 +0200

    syntax

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "environment" {
-  value = "${var.environment}"
+  value = var.environment
 }
 
 output "vpc_cidr" {
-  value = "${var.vpc_cidr}"
+  value = var.vpc_cidr
 }
 
 output "admin_key_name" {
-  value = "${aws_key_pair.admin_key.key_name}"
+  value = aws_key_pair.admin_key.key_name
 }
 
 output "private_subnets" {
-  value = [ "${aws_subnet.private.*.id}" ]
+  value = [ aws_subnet.private.*.id ]
 }
 
 output "public_subnets" {
-  value = [ "${aws_subnet.public.*.id}" ]
+  value = [ aws_subnet.public.*.id ]
 }
 
 output "vpc_id" {
