f2b819a9840a5c4ced92d9ae83ef9b1251a09376
Ben Saada <bensaada12@gmail.com>
2021-03-25 03:11:12 +0200

    First Commit

--- modules/site-modules.tf
+++ modules/site-modules.tf
@@ -2,31 +2,31 @@
 // define an s3 backend here
 terraform {
   backend "s3" {
-    bucket = "workshop-tf-state"
-    key = "workshop-site-state/terraform.tfstate"
+    bucket         = "danishemeshterraform"
+    key            = "workshop-site-state/terraform.tfstate"
     dynamodb_table = "tf-workshop-site-locks"
-    region = "???"
+    region         = "us-east-2"
   }
 }
 
 
 provider "aws" {
-  region = "???"
+  region = "us-east-2"
 }
 
 module "vpc" {
   source = "../"
 
-  environment = "workshop-production"
-  region = "???"
-  vpc_cidr =  "172.???.???.???/???"
-  private_subnets = "172.???.???.???/???"  
-  public_subnets  = "172.???.???.???/???"  
+  environment     = "workshop-production"
+  region          = "us-east-2"
+  vpc_cidr        = "172.18.0.0/18"
+  private_subnets = "172.18.0.0/20,172.18.16.0/20"
+  public_subnets  = "172.18.32.0/20,172.18.48.0/20"
 
   //fill 2 availability zones associated with the region
-  azs  =  "???, ???"
+  azs = "us-east-2a,us-east-2b"
 
-  enable_dns_support = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
 }
@@ -45,16 +45,16 @@ output "admin_key_name" {
 
 
 output "private_subnets" {
-value = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 }
 
 
 output "public_subnets" {
-  value = ???
+  value = module.vpc.public_subnets
 }
 
 output "vpc_id" {
-  value = ???
+  value = module.vpc.vpc_id
 }
 
 
